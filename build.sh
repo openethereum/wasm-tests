17c5d101b3f571c4bfe88e566eeefa421f3abf11
NikVolf <nikvolf@gmail.com>
2017-05-30 13:39:01 +0300

    update submodule

--- build.sh
+++ build.sh
@@ -6,5 +6,5 @@ output_file=$2
 wasm_file=$3
 
 rustc $input_file -o $output_file -O --target wasm32-unknown-emscripten -C linker=./linker_emcc.sh
-cargo run --manifest-path=./wasm-tools/ext/Cargo.toml --release -- $wasm_file $wasm_file
-cargo run --manifest-path=./wasm-tools/opt/Cargo.toml --release -- $wasm_file $wasm_file
+cargo run --manifest-path=./wasm-utils/ext/Cargo.toml --release -- $wasm_file $wasm_file
+cargo run --manifest-path=./wasm-utils/opt/Cargo.toml --release -- $wasm_file $wasm_file
