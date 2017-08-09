725530f610c3f325fff36d6f31b1bec1531b8c2c
NikVolf <nikvolf@gmail.com>
2017-08-10 01:10:22 +0300

    refactoring continued

--- build.sh
+++ build.sh
@@ -4,5 +4,6 @@ cargo build --release --bins --verbose --target wasm32-unknown-emscripten
 wasm-build ./target empty
 wasm-build ./target suicidal
 wasm-build ./target realloc
+wasm-build ./target logger
 
 cp ./target/*.wasm ./compiled
