15b439de030ea4e7fcefb1a224867b67a13cbed0
fro <frol.rage@gmail.com>
2017-09-12 19:47:54 +0300

    rename sha3 -> keccak

--- src/keccak.rs
+++ src/keccak.rs
@@ -3,12 +3,12 @@
 
 extern crate pwasm_std;
 
-use pwasm_std::{CallArgs, sha3};
+use pwasm_std::{CallArgs, keccak};
 
 #[no_mangle]
 pub fn call(desc: *mut u8) {
     let mut ctx = unsafe { CallArgs::from_raw(desc) };
-    let res = sha3("");
+    let res = keccak("");
     *ctx.result_mut() = res.0.to_vec().into_boxed_slice();
     unsafe { ctx.save(desc); }
 }
