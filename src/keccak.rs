fa6681d578da0178e122aeecb2b69e34397d4c6b
fro <frol.rage@gmail.com>
2017-09-13 13:58:31 +0300

    keccak test uses data param as an input for the keccak hash function

--- src/keccak.rs
+++ src/keccak.rs
@@ -8,7 +8,7 @@ use pwasm_std::{CallArgs, keccak};
 #[no_mangle]
 pub fn call(desc: *mut u8) {
     let mut ctx = unsafe { CallArgs::from_raw(desc) };
-    let res = keccak("");
+    let res = keccak(ctx.params().args());
     *ctx.result_mut() = res.0.to_vec().into_boxed_slice();
     unsafe { ctx.save(desc); }
 }
