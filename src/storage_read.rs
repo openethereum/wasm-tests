138428a67fa79032a825fe6eca703f30f76d9182
Sergey Pepyakin <s.pepyakin@gmail.com>
2017-11-20 17:14:10 +0300

    Remove storage Error

--- src/storage_read.rs
+++ src/storage_read.rs
@@ -6,7 +6,7 @@ extern crate pwasm_std;
 use pwasm_std::{storage, write_u32};
 use pwasm_std::hash::H256;
 
-fn get_value_from_key(key: u32) -> Result<[u8; 32], storage::Error> {
+fn get_value_from_key(key: u32) -> [u8; 32] {
     let mut full_key = [0u8; 32];
     write_u32(&mut full_key[0..4], key);
     storage::read(&H256::from(full_key))
@@ -15,10 +15,6 @@ fn get_value_from_key(key: u32) -> Result<[u8; 32], storage::Error> {
 #[no_mangle]
 pub fn call(desc: *mut u8) {
     let (_, result) = unsafe { pwasm_std::parse_args(desc) };
-    let val: [u8; 32] = match get_value_from_key(1) {
-        Ok(v) => v,
-        Err(_) => [0u8; 32]
-    };
-
+    let val: [u8; 32] = get_value_from_key(1);
     result.done(val.to_vec());
 }
