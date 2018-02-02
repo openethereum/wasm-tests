458a9d43b2027b802bb66b1b5f1a10e2fd755b38
NikVolf <nikvolf@gmail.com>
2018-02-03 01:34:25 +0300

    logger, externs, realloc

--- src/realloc.rs
+++ src/realloc.rs
@@ -1,18 +1,16 @@
-#![no_main]
 #![no_std]
 
 extern crate pwasm_std;
+extern crate pwasm_ethereum as ext;
 
 use pwasm_std::Vec;
 
 #[no_mangle]
-pub fn call(desc: *mut u8) {
-    let (input, result) = unsafe { pwasm_std::parse_args(desc) };
-
-    result.done({
+pub fn call() {
+    ext::ret(&{
         let mut data = Vec::with_capacity(1);
         data.push(0u8);
-        for arg in input.as_ref() {
+        for arg in ext::input().iter() {
             data.push(*arg);
         }
         data
