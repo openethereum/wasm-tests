6559f9d8da2d85c5f19315402eec151c55317d3f
NikVolf <nikvolf@gmail.com>
2018-02-03 01:59:15 +0300

    travis and build

--- build.sh
+++ build.sh
@@ -1,18 +1,18 @@
 #!/bin/sh
 
-./build-test.sh call
-./build-test.sh call_code
-./build-test.sh call_static
-./build-test.sh creator
-./build-test.sh dispersion
-./build-test.sh empty
-./build-test.sh externs
-./build-test.sh events
-./build-test.sh identity
-./build-test.sh logger
-./build-test.sh realloc
-./build-test.sh rterr
-./build-test.sh keccak
-./build-test.sh suicidal
-./build-test.sh storage_read
+./build-test.sh call &&
+./build-test.sh call_code &&
+./build-test.sh call_static &&
+./build-test.sh creator &&
+./build-test.sh dispersion &&
+./build-test.sh empty &&
+./build-test.sh externs &&
+./build-test.sh events &&
+./build-test.sh identity &&
+./build-test.sh logger &&
+./build-test.sh realloc &&
+./build-test.sh rterr &&
+./build-test.sh keccak &&
+./build-test.sh suicidal &&
+./build-test.sh storage_read &&
 ./build-test.sh math
