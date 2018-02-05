4ff0e8eef84b86682cdcb91587cc2238df84a9d9
NikVolf <nikvolf@gmail.com>
2018-02-05 18:33:55 +0300

    recompile with reduced stack requirements

--- build-test.sh
+++ build-test.sh
@@ -3,7 +3,7 @@
 TEST_NAME=$1
 
 cargo run --manifest-path ./gen/Cargo.toml -- $TEST_NAME &&
-RUSTFLAGS=-g CARGO_TARGET_DIR=./target cargo build --manifest-path=./target/tests/$TEST_NAME/Cargo.toml --release --target=wasm32-unknown-unknown &&
+CARGO_TARGET_DIR=./target cargo build --manifest-path=./target/tests/$TEST_NAME/Cargo.toml --release --target=wasm32-unknown-unknown &&
 ../wabt/wasm2wat -f ./target/wasm32-unknown-unknown/release/$TEST_NAME.wasm -o ./target/$TEST_NAME-unopt.wat
 wasm-build ./target $TEST_NAME --target wasm32-unknown-unknown &&
 cp ./target/$TEST_NAME.wasm ./compiled
