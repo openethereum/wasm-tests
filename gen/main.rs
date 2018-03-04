39ab8ae9747c24052c895699426a8d215b44fb04
fro <frol.rage@gmail.com>
2018-03-04 18:19:43 +0100

    pwasm-std update, pwasm-ethereum update

--- gen/main.rs
+++ gen/main.rs
@@ -17,29 +17,24 @@ version = "0.1.0"
 authors = ["NikVolf <nikvolf@gmail.com>"]
 
 [dependencies]
-pwasm-std = { git = "https://github.com/paritytech/pwasm-std", branch = "use-rlibc" }
-pwasm-ethereum = { git = "https://github.com/nikvolf/pwasm-ethereum" }
+pwasm-std = "0.5.0"
+pwasm-ethereum = "0.1.0"
 bigint = { version = "4", default-features = false }
 
 [lib]
 name = "$file_name"
 path = "main.rs"
 crate-type = ["cdylib"]
-
-[replace]
-"pwasm-std:0.3.2" = { git = "https://github.com/paritytech/pwasm-std", branch = "use-rlibc" }
-	"#;
+"#;
 
 	let target_toml = toml.replace("$file_name", file_name);
 
-	let mut toml_path = PathBuf::from("target");
-	toml_path.push("tests");
-	toml_path.push(file_name);
+	let mut crate_dir_path = PathBuf::from("target");
+	crate_dir_path.push("tests");
+	crate_dir_path.push(file_name);
 
-	fs::create_dir_all(&toml_path).expect(&format!("failed to create \"{}\" directory", toml_path.to_string_lossy()));
-	let target_dir = toml_path.clone();
-
-	toml_path.push("Cargo.toml");
+	fs::create_dir_all(&crate_dir_path).expect(&format!("failed to create \"{}\" directory", crate_dir_path.to_string_lossy()));
+	let target_dir = crate_dir_path.clone();
 
 	let mut source_path = PathBuf::from("src");
 	source_path.push(&format!("{}.rs", file_name));
@@ -51,8 +46,16 @@ crate-type = ["cdylib"]
 		&format!("failed to copy {} to {}", source_path.to_string_lossy(), target_main_path.to_string_lossy()));
 
 	{
+		let mut toml_path = crate_dir_path.clone();
+		toml_path.push("Cargo.toml");
 		let mut f = fs::File::create(toml_path.clone()).expect(&format!("failed to create \"{}\" file", toml_path.to_string_lossy()));
 		::std::io::Write::write_all(&mut f, &target_toml.as_bytes()[..])
+			.expect("Failed to write toml");
+
+		let mut rust_toolchain_path = crate_dir_path.clone();
+		rust_toolchain_path.push("rust-toolchain");
+		let mut f = fs::File::create(rust_toolchain_path.clone()).expect(&format!("failed to create \"{}\" file", rust_toolchain_path.to_string_lossy()));
+		::std::io::Write::write_all(&mut f, &"nightly-2018-02-05".as_bytes()[..])
 			.expect("Failed to write toml");
 	}
 }
