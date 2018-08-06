a2202e0ba9434533cc7483e75a3c9b2c7781c34e
Wei Tang <hi@that.world>
2018-08-06 17:17:26 +0800

    Use raw module

--- build-rust-test.sh
+++ build-rust-test.sh
@@ -7,5 +7,5 @@ ADDITIONAL_FEATURES=$2
 
 cargo run --manifest-path ./gen/Cargo.toml -- $TEST_NAME $ADDITIONAL_FEATURES
 RUSTFLAGS="-C link-arg=-z  -C link-arg=stack-size=65536" CARGO_TARGET_DIR=./target cargo build --manifest-path=./target/tests/$TEST_NAME/Cargo.toml --release --target=wasm32-unknown-unknown
-wasm-build ./target $TEST_NAME --target wasm32-unknown-unknown
-cp ./target/$TEST_NAME.wasm ./compiled
+wasm-build ./target $TEST_NAME --save-raw ./target/$TEST_NAME.raw.wasm --target wasm32-unknown-unknown
+cp ./target/$TEST_NAME.raw.wasm ./compiled/$TEST_NAME.wasm
