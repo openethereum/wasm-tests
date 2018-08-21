87d8e84a36a8fa327b57c6b60322adb34204a2ff
fro <frol.rage@gmail.com>
2018-08-21 17:53:09 +0400

    call_gasleft test added

---
+++ src/call_gasleft.rs
@@ -0,0 +1,27 @@
+#![no_std]
+
+#[macro_use] extern crate pwasm_std;
+extern crate pwasm_ethereum;
+
+use pwasm_std::logger;
+use pwasm_std::hash::Address;
+use pwasm_ethereum::{call as ext_call, gas_left as ext_gasleft, ret};
+
+#[no_mangle]
+pub fn call() {
+	let addr = Address::from([99, 88, 77, 66, 55, 44, 33, 22, 11, 0, 11, 22, 33, 44, 55, 66, 77, 88, 99, 0]);
+
+	let input = [129u8, 123, 113, 107, 101, 97];
+	let mut temp = vec![0u8; 256];
+	// 700 is the cost of CALL
+	match ext_call(ext_gasleft() - 700, &addr, 1000000000.into(), &input, &mut temp[..]) {
+		Ok(_) => {
+			logger::debug("Call succeed");
+		},
+		Err(_) => {
+			logger::debug("Call failed");
+		}
+	}
+
+	ret(&[]);
+}
